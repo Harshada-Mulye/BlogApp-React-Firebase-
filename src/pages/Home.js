cdb86b9731862e5634e0fbec087a76f8c7c7812a
jayashreeithse <jayashreebathala@gmail.com>
2022-05-17 20:20:04 +0200

    added carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
 import React from "react";
 import HomepageImg from "../assets/images/HomepageImg.png";
+import pic1 from "../assets/images/pic1.jpg";
+import pic2 from "../assets/images/pic2.jpg";
+import pic3 from "../assets/images/pic3.jpg";
 import Carousel from 'react-bootstrap/Carousel' 
 
 const Home = () => {
   return (
-    <div>
+    <div className="carousel">
       {/* <img src={HomepageImg} /> */}
       <Carousel>
   <Carousel.Item>
-    <img
+    <img 
       className="d-block w-100"
-      src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      src={pic1}
       alt="First slide"
     />
     <Carousel.Caption>
@@ -21,7 +24,7 @@ const Home = () => {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://images.unsplash.com/photo-1504150558240-0b4fd8946624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+      src={pic2}
       alt="Second slide"
     />
 
@@ -33,7 +36,7 @@ const Home = () => {
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=780&q=80"
+      src={pic3}
       alt="Third slide"
     />
 
@@ -42,6 +45,7 @@ const Home = () => {
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
+
 </Carousel>
     </div>
   );
